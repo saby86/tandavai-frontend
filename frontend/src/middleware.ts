8aa7fde1f37cfa94dfa94316030ec7ec2d4874f3
Francis <uig46926@automotive-wan.com>
2025-11-24 10:16:48 -0600

    FIX: Add Clerk Middleware and update API URL config

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,14 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher([
+    '/dashboard(.*)',
+    '/api(.*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+    if (isProtectedRoute(req)) (await auth()).protect();
+});
+
+export const config = {
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
